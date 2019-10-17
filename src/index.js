acc0aef8ac53bb66e1abc289c6df62b4754de4a7
Bailey Pownell <bailey.pownell@gmail.com>
2019-10-17 14:46:33 -0400

    configured so that webpack is working correctly and SASS is being transpiled into CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/main.scss';
+
+const title = 'React with Webpack and Babel';
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
+    ]
+  }
+};
