cce2a76b538130465edb54f2d4156bc1874886d2
Bailey Pownell <bailey.pownell@gmail.com>
2019-10-22 10:35:58 -0400

    modified login and account sign up page

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import Content from './Content';
 
-export default function CreateAccount() {
-  return <Content>
-    <h1>CREATE AN ACCOUNT</h1>
-    <form>
-      <label><h2>First Name:</h2></label>
-      <input type="text" name="first_name">
-      </input>
-      <label><h2>Last Name:</h2></label>
-      <input type="text" name="last_name">
-      </input>
-      <label><h2>Email:</h2></label>
-      <input type="email" name="email">
-      </input>
-      <button>SIGN UP <i class="fas fa-arrow-circle-right"></i></button>
-    </form>
-  </Content>
+export default class CreateAccount extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <Content>
+        <h1>CREATE AN ACCOUNT</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label><h2>First Name:</h2></label>
+          <input type="text" name="first_name" id="firstName" onChange={this.handleChange}>
+          </input>
+          <label><h2>Last Name:</h2></label>
+          <input type="text" name="last_name" id="lastName" onChange={this.handleChange}>
+          </input>
+          <label><h2>Email:</h2></label>
+          <input type="email" name="email" id="email" onChange={this.handleChange}>
+          </input>
+          <label><h2>Password:</h2></label>
+          <input type="password" name="password" id="password" onChange={this.handleChange}>
+          </input>
+          <button>SIGN UP <i class="fas fa-arrow-circle-right"></i></button>
+        </form>
+      </Content>
+    )
+  }
 }
---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "AIzaSyBa2yI5F5kpQTAJAyACoxkA5UyCfaEM7Pk",
+  authDomain: "weight-tracker-91762.firebaseapp.com",
+  databaseURL: "https://weight-tracker-91762.firebaseio.com",
+  projectId: "weight-tracker-91762",
+  storageBucket: "weight-tracker-91762.appspot.com",
+  messagingSenderId: "325881243868",
+};
