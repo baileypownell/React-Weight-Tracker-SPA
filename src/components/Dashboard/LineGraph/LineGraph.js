071d0e22bd5cf6b45212d2a22130226ca29ca824
Bailey Pownell <bailey.pownell@gmail.com>
2021-06-28 13:01:37 -0400

    introduced luxon for date formatting, added evergreen-ui for table update and accurate update of editing user's current weight and seeing that reflected in the table

--- src/components/Dashboard/LineGraph/LineGraph.js
+++ src/components/Dashboard/LineGraph/LineGraph.js
@@ -3,7 +3,6 @@ import './LineGraph.scss'
 import { connect } from 'react-redux'
 import M from 'materialize-css'
 
-
 let myChart
 
 class LineGraph extends React.Component {
@@ -14,7 +13,6 @@ class LineGraph extends React.Component {
   }
 
   componentDidMount() {
-
     let el = document.querySelector('.tabs')
     setTimeout(() => {
       M.Tabs.init(el);
@@ -32,7 +30,7 @@ class LineGraph extends React.Component {
       }]
     }
 
-    var ctx = document.getElementById('myChart');
+    const ctx = document.getElementById('myChart');
     myChart = new Chart(ctx, {
       type: 'line', 
       data, 
@@ -87,57 +85,57 @@ class LineGraph extends React.Component {
     myChart.update()
   }
 
-    prepareChartData(num) {
-      const { weights } = this.props
-      if (!weights.length) {
-        return
-      } 
-      let labels = [];
-      let data = [];
-      let newerThanTime = [];
-      for (let i = 0; i < weights.length; i++) {
-          const now = new Date()
-          const secondsSinceEpoch = Math.round(now.getTime() / 1000);
-          let timeLengthAgo = secondsSinceEpoch - num;
-          if (weights[i].date.date.seconds > timeLengthAgo) {
-            newerThanTime.push(weights[i]);
-          }
-      }
-
-      newerThanTime.forEach((item) => {
-          data.push(item.weight);
-          let date = new Date(item.date.date.seconds * 1000);
-          labels.push(date)
-      })
-
-      labels.reverse()
-      data.reverse()
-
-      let labelsParsed = [];
-      labels.forEach(date => {
-          let day = date.toUTCString().split(' ')[1];
-          let month = date.toUTCString().split(' ')[2];
-          let fulldate = [month, day].join(' ');
-          labelsParsed.push(fulldate);
-      })
-      this.drawChart(labelsParsed, data) 
+  prepareChartData(num) {
+    const { weights } = this.props
+    if (!weights.length) {
+      return
+    } 
+    let labels = [];
+    let data = [];
+    let newerThanTime = [];
+    for (let i = 0; i < weights.length; i++) {
+        const now = new Date()
+        const secondsSinceEpoch = Math.round(now.getTime() / 1000);
+        let timeLengthAgo = secondsSinceEpoch - num;
+        if (weights[i].date.date.seconds > timeLengthAgo) {
+          newerThanTime.push(weights[i]);
+        }
     }
 
-    graphData = () => {
-      const { graphTimePeriod } = this.state
-      if (graphTimePeriod === 'week') {
-        this.prepareChartData(604800)
-      } else if (graphTimePeriod === 'month') {
-        this.prepareChartData(2592000)
-      } else if (graphTimePeriod === 'year') {
-          this.prepareChartData(31556952)
-      }
+    newerThanTime.forEach((item) => {
+        data.push(item.weight);
+        let date = new Date(item.date.date.seconds * 1000);
+        labels.push(date)
+    })
+
+    labels.reverse()
+    data.reverse()
+
+    let labelsParsed = [];
+    labels.forEach(date => {
+        let day = date.toUTCString().split(' ')[1];
+        let month = date.toUTCString().split(' ')[2];
+        let fulldate = [month, day].join(' ');
+        labelsParsed.push(fulldate);
+    })
+    this.drawChart(labelsParsed, data) 
+  }
+
+  graphData = () => {
+    const { graphTimePeriod } = this.state
+    if (graphTimePeriod === 'week') {
+      this.prepareChartData(604800)
+    } else if (graphTimePeriod === 'month') {
+      this.prepareChartData(2592000)
+    } else if (graphTimePeriod === 'year') {
+        this.prepareChartData(31556952)
     }
+  }
 
   render() {
     return (   
       <div id="line-graph">   
-          { this.props.weights.length ? 
+        { this.props.weights.length ? 
           <div className="white-box" id="charts">
             <h6>Your weight over the past: </h6>
             <div>
@@ -154,8 +152,7 @@ class LineGraph extends React.Component {
               <canvas id="myChart" width="400" height="400"></canvas>
             </div>
           </div>
-          : null
-          }
+          : null}
       </div>
     )
   }
