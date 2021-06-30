f43359bf2c188c2e81b2b3dfb69b8b1661c67cdf
Bailey Pownell <bailey.pownell@gmail.com>
2021-06-30 11:54:36 -0400

    homepage update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,6 +18,17 @@ class Home extends React.Component {
       <div id="mobile-center">
         <div className="content-parent">
           <h5>It's never been <br/><span id="fancy">easier</span><br/> to track your progress.</h5>
+
+          <div id="features">
+            <p id="logo-font">happy balance</p>
+            <ul>
+              <li>is simple to use <i class="fas fa-check-circle"></i></li>
+              <li>enables you to set goals for yourself <i class="fas fa-check-circle"></i></li>
+              <li>and most importantly, is completely <span id="tiny">free</span> <i class="fas fa-check-circle"></i></li>
+            </ul>
+          </div>
+          
+
           <button 
             className="waves-effect waves-light btn" 
             onClick={this.directUser}>get started</button>
